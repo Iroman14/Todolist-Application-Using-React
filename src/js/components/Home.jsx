81fe52a436a33a90797767356cc15bf9e19ee7c0
Iroman14 <134985621+Iroman14@users.noreply.github.com>
2025-02-26 10:39:54 +0000

    Proyecto final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,57 @@
-import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "bootstrap";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function Home() {
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState("");
+	const [show, setShow] = useState(null);
 
-//create your first component
-const Home = () => {
+	const AddTask = () => {
+		if (newTask !== "") {
+			setTasks([...tasks, newTask]);
+			setNewTask("");
+		}
+	};
+
+	const RemoveTask = (index) => {
+		setTasks(tasks.filter((_, remove) => remove !== index));
+	};
+
 	return (
-		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="Container">
+			<h1>To Do List</h1>
+			<ul>
+				<li>
+					<input
+						type="text"
+						placeholder="Deseas agregar alguna nota?"
+						onChange={(note) => setNewTask(note.target.value)}
+						value={newTask}
+						onKeyDown={(note) => {
+							if (note.key === "Enter") {
+								AddTask();
+							}
+						}}
+						className="form-control">
+					</input>
+				</li>
+				{tasks.length === 0 ? (
+					<li> No hay tareas pendientes, añadir tareas nuevas </li>
+				) : (
+					tasks.map((note, index) => (
+						<li onMouseEnter={() => setShow(index)} onMouseLeave={() => setShow(null)} key={index} className="d-flex justify-content-between align-items-center">
+							{note}
+							{show === index && (<button onClick={() => RemoveTask(index)} className="btn btn-danger"><FontAwesomeIcon icon={faTrash} /></button>)}
+						</li>
+					)
+					))}
+			</ul>
+			<button onClick={AddTask} className="btn btn-dark"> Añadir Nota </button>
+			<div className="Counter">{tasks.length} tasks</div>
 		</div>
-	);
-};
+	)
+}
 
 export default Home;
